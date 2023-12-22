086682cb36f1604d3c95bd6dc651664d6ad9a32d
Christian Sifuentes <me@radixpoint.dev>
2023-12-22 14:55:29 -0500

    new pages for breakdown and monthly

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 // import Dashboard from "components/Navigate";
 
 function App() {
@@ -33,6 +35,8 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
